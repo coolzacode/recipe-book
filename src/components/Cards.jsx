dda29b51e6615d6c4dc6c6db70ce545e630264f6
hazzaah <154761475+hazzaah@users.noreply.github.com>
2025-06-30 09:03:25 -0500

    import heart svg and remove favorites array

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,24 +1,16 @@
+import { useState } from 'react'
 import '../styles/cards.css'
 import Recipes from '../recipes.json'
 
 function Cards() {
-  let favorites = [];
-
-  function handleClick(id){
-    if(favorites.includes(id)){
-      favorites = favorites.filter(favorite => favorite !== id);
-    }else {
-      favorites.unshift(id);
-    }
-    console.log(favorites);
-  }
+  // const [favorite, setFavorite] = useState([]);
 
   return (
     <main>
       {Recipes && Recipes.map( recipe => (
         <div key={recipe.id} className="card">
           <div className="favorite">
-            <p onClick={() => handleClick(recipe.id)}>♡</p>
+            <span className="material-symbols-outlined">favorite</span>
           </div>
           <div className="info">
             <p className="category">{recipe.category}</p>
