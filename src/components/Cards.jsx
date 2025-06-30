2b319b1971fb42bc764d1f7fdf96ee8974a8680a
hazzaah <154761475+hazzaah@users.noreply.github.com>
2025-06-30 10:07:10 -0500

    add favorites array again and toggle favorites svg

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import '../styles/cards.css'
 import Recipes from '../recipes.json'
 
 function Cards() {
-  // const [favorite, setFavorite] = useState([]);
+  const iconRefs = useRef([]);
+  let favorites = [];
+
+  function handleClick(id, index) {
+    if(favorites.includes(id)){
+      favorites = favorites.filter(favorite => favorite !== id);
+    } else {
+      favorites.unshift(id);
+    }
+    console.log(favorites);
+    const el = iconRefs.current[index];
+    if(el) {
+      el.classList.toggle('filled');
+    }
+  }
 
   return (
     <main>
-      {Recipes && Recipes.map( recipe => (
+      {Recipes && Recipes.map( (recipe, index) => (
         <div key={recipe.id} className="card">
           <div className="favorite">
-            <span className="material-symbols-outlined">favorite</span>
+            <span ref={el => (iconRefs.current[index] = el)} onClick={() => handleClick(recipe.id, index)} className={"material-symbols-outlined"}>favorite</span>
           </div>
           <div className="info">
             <p className="category">{recipe.category}</p>
