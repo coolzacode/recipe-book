e632fa260c8b6ff4378ddd88020efbb2e33604f7
hazzaah <154761475+hazzaah@users.noreply.github.com>
2025-07-01 07:27:03 -0500

    update nav styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,8 @@ import '../styles/nav.css'
 function Nav() {
   return (
     <nav>
-      <h1><Link to="/">All Recipes</Link></h1>
-      <h1><Link to="/favorites">♡ Favorites</Link></h1>
+      <h1><Link to="/" className="link">All Recipes</Link></h1>
+      <h1><Link to="/favorites" className="link">♡ Favorites</Link></h1>
     </nav>
   )
 }
