35c2334a7161f3410ca820012a4c95380bdbc4ef
hazzaah <154761475+hazzaah@users.noreply.github.com>
2025-07-02 06:29:45 -0500

    fix scrolling issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import Nav from './components/Nav.jsx'
-import Cards from './components/Cards.jsx'
 import Favorites from './components/Favorites.jsx'
 import Home from './pages/Home.jsx'
 
